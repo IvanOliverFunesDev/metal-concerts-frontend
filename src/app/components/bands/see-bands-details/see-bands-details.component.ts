c40946623e789953a9c677b45619a8d08591bea0
Ivan-Adaits <ioliverfunes@adaits.es>
2025-01-19 15:19:59 +0100

    feat: integrate ConcertsListComponent into Band Details view

--- src/app/components/bands/see-bands-details/see-bands-details.component.ts
+++ src/app/components/bands/see-bands-details/see-bands-details.component.ts
@@ -2,36 +2,28 @@ import { Component } from '@angular/core';
 import { Band } from '../../../interfaces/band';
 import { ConcertsService } from '../../../services/concerts.service';
 import { ActivatedRoute } from '@angular/router';
-import { NgFor, NgIf } from '@angular/common';
-import { Concert } from '../../../interfaces/concert';
+import { NgIf } from '@angular/common';
+import { ConcertsListComponent } from "../../concerts/concerts-list/concerts-list.component";
 
 @Component({
   selector: 'app-see-bands-details',
-  imports: [NgIf, NgFor],
+  imports: [NgIf, ConcertsListComponent],
   templateUrl: './see-bands-details.component.html',
   styleUrl: './see-bands-details.component.css'
 })
 export class SeeBandsDetailsComponent {
   band: Band | null = null;
-  concerts: Concert[] = [];
-  constructor(private route: ActivatedRoute, private concertsService: ConcertsService) { }
-  ngOnInit(): void {
 
+  constructor(private route: ActivatedRoute, public concertsService: ConcertsService) { }
+
+  ngOnInit(): void {
     const bandId = this.route.snapshot.paramMap.get('id');
 
     if (bandId) {
       this.concertsService.getBandById(bandId).subscribe({
-        next: (data) => { this.band = data },
-        error: (err) => { console.error('Error cargando el concierto:', err); }
-      })
-
-    }
-    if (bandId) {
-      this.concertsService.getConcertsByBandId(bandId).subscribe({
-        next: (data) => { this.concerts = data },
-        error: (err) => { console.error('Error cargando el concierto:', err); }
-      })
-
+        next: (data) => this.band = data,
+        error: (err) => console.error('Error cargando la banda:', err)
+      });
     }
   }
 }
--- src/app/components/concerts/concerts-list/concerts-list.component.ts
+++ src/app/components/concerts/concerts-list/concerts-list.component.ts
@@ -1,4 +1,4 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { ConcertsService } from '../../../services/concerts.service';
@@ -7,16 +7,15 @@ import { Concert } from '../../../interfaces/concert';
 
 @Component({
   selector: 'app-concerts-list',
-  imports: [NgFor, ConcertsCardComponent],
+  imports: [NgFor, ConcertsCardComponent, NgIf],
   templateUrl: './concerts-list.component.html',
   styleUrl: './concerts-list.component.css'
 })
 export class ConcertsListComponent implements OnInit, OnDestroy {
   @Input() fetchData!: () => Observable<Concert[]>;
-
   @Input() title: string = '';
-
   concerts: Concert[] = [];
+  hasConcerts: boolean | null = null;
 
   private subscription!: Subscription;
 
@@ -26,9 +25,11 @@ export class ConcertsListComponent implements OnInit, OnDestroy {
     this.subscription = this.fetchData().subscribe({
       next: (data) => {
         this.concerts = data;
+        this.hasConcerts = data.length > 0; // Si hay conciertos, true. Si no, false.
       },
       error: (err) => {
         console.error('Error cargando conciertos:', err);
+        this.hasConcerts = false; // Si hay un error, asumimos que no hay conciertos.
       }
     });
   }
