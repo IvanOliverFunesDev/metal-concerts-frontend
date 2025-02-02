9619dfbb2d35464c995c11d70e966864d5167460
Ivan-Adaits <ioliverfunes@adaits.es>
2025-02-02 01:03:58 +0100

    prueba

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { gsap } from 'gsap';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule, NgIf, RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  isMobile: boolean = false;
+  menuOpen: boolean = false;
 
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe(['(max-width: 768px)']).subscribe(result => {
+      this.isMobile = result.matches;
+    });
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+
+    if (this.menuOpen) {
+      gsap.to('.side-menu', { right: 0, duration: 0.3, ease: 'power2.out' });
+      gsap.to('.menu-overlay', { opacity: 1, display: 'block', duration: 0.3 });
+    } else {
+      gsap.to('.side-menu', { right: '-300px', duration: 0.3, ease: 'power2.in' });
+      gsap.to('.menu-overlay', { opacity: 0, display: 'none', duration: 0.3 });
+    }
+  }
 }
