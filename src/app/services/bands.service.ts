01584bcd54d9f1cd6a119ece19f6a9d109d107b8
Ivan-Adaits <ioliverfunes@adaits.es>
2025-02-02 03:53:50 +0100

    feat: band populate

--- src/app/services/bands.service.ts
+++ src/app/services/bands.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map, Observable, of } from 'rxjs';
-import { BandList } from '../interfaces/band';
+import { BandList, BandPopulate } from '../interfaces/band';
 import { BandPublic } from '../interfaces/band-profile-public';
 
 @Injectable({
@@ -10,6 +10,7 @@ import { BandPublic } from '../interfaces/band-profile-public';
 export class BandsService {
   private apiUrl = 'http://localhost:3000/api/bands';
   constructor(private http: HttpClient) { }
+
   getBandsAll(): Observable<BandList[]> {
     return this.http.get<{ success: boolean, message: string, data: BandList[] }>(this.apiUrl).pipe(
       map(response => response.data),
@@ -19,6 +20,7 @@ export class BandsService {
       })
     )
   }
+
   getBandById(id: string): Observable<BandPublic | null> {
     return this.http.get<{ success: boolean, message: string, data: BandPublic }>(`${this.apiUrl}/${id}`).pipe(
       map(response => response.data),
@@ -27,6 +29,19 @@ export class BandsService {
         return of(null)
       })
     )
+  }
+
+  getBandsPopular(limit: number = 0): Observable<BandList[]> {
+    const params = new HttpParams()
+      .set('limit', limit > 0 ? limit.toString() : 'all');
+
+    return this.http.get<{ success: boolean, message: string, data: BandList[] }>(`${this.apiUrl}/popular`, { params }).pipe(
+      map(response => response.data),
+      catchError(error => {
+        console.log('Error obteniendo bandas populares:', error);
+        return of([]);
+      })
+    );
   }
 }
 
